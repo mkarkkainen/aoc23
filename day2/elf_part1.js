e22fa81b832f891507c2ad6d843766067997415e
Miikka K <miikkak@tuta.io>
2023-12-02 21:43:30 -0600

    cleanup

--- day2/elf_part1.js
+++ day2/elf_part1.js
@@ -1,5 +1,5 @@
 //   --- Day 2: Cube Conundrum ---
-
+//         --- Part One ---
 // which games would have been possible if the bag contained only 12 red cubes,
 // 13 green cubes, and 14 blue cubes?
 //
